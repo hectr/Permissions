87e6f6cd066c831a9ab47251788ad6de612a8612
hectr <h@mrhector.me>
2020-05-03 22:59:00 +0200

    Use different strategy for determining location depending on the source's subject

--- Sources/Permissions/sources/LocationManager/LocationManagerAuthorizationSource.swift
+++ Sources/Permissions/sources/LocationManager/LocationManagerAuthorizationSource.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct LocationManagerAuthorizationSource: AuthorizationSource, AuthorizationStatusSource {
-    public enum When {
+    public enum When: Equatable, CaseIterable {
         case always
         case inUse
     }
@@ -14,16 +14,69 @@ public struct LocationManagerAuthorizationSource: AuthorizationSource, Authoriza
         provider.status
     }
 
+    public var determineAlwaysPromptTimeout: TimeInterval = 40
+
     private let provider: LocationAuthorizationProvider
 
     public init(subject: When) {
         provider = LocationAuthorizationProvider(subject: subject)
     }
 
+    public func determineAuthorizationStatus(completion: @escaping (AuthorizationStatus<Subject>) -> Void) {
+        switch self.subject {
+        case .always:
+            determineAlwaysAuthorizationStatus(timeout: determineAlwaysPromptTimeout,
+                                               completion: completion)
+
+        case .inUse:
+            determineInUseAuthorizationStatus(completion: completion)
+        }
+    }
+
     public func requestAuthorization(timeout: TimeInterval?, handler: @escaping (Result<Void, Swift.Error>) -> Void) {
         let completion = ExpirableCompletion(timeout: timeout, operationQueue: operationQueue, completion: handler)
         provider.requestAuthorization { error in
             completion.execute(with: error)
+        }
+    }
+
+    private func determineAlwaysAuthorizationStatus(timeout: TimeInterval, completion: @escaping (AuthorizationStatus<Subject>) -> Void) {
+        determineInUseAuthorizationStatus { status in
+            switch status {
+            case let .authorized(subject):
+                if subject == self.subject {
+                    completion(status)
+                } else {
+                    self.requestAuthorization(timeout: timeout) { _ in
+                        self.getStatus(completion: completion)
+                    }
+                }
+
+            case let .unauthorized(reason):
+                self.handleUnauthorized(reason: reason, completion: completion)
+            }
+        }
+    }
+
+    private func determineInUseAuthorizationStatus(completion: @escaping (AuthorizationStatus<Subject>) -> Void) {
+        getStatus { status in
+            switch status {
+            case .authorized:
+                completion(status)
+
+            case let .unauthorized(reason):
+                self.handleUnauthorized(reason: reason, completion: completion)
+            }
+        }
+    }
+
+    private func handleUnauthorized(reason: AuthorizationStatus<When>.Reason, completion: @escaping (AuthorizationStatus<Subject>) -> Void) {
+        if reason.canRequestPermission {
+            self.requestAuthorization(timeout: nil) { _ in
+                self.getStatus(completion: completion)
+            }
+        } else {
+            completion(status)
         }
     }
 }
